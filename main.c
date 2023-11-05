0c77a6a0144b68a6aef1248e2e345477edfa6ec2
99daeun <lucy8631@naver.com>
2023-11-05 21:38:00 +0900

    3

--- main.c
+++ main.c
@@ -6,20 +6,17 @@
 int main(int argc, char *argv[]) {
 	
 	int grade[N_STUDENT];
-	int i, average;
-	int sum;
+	int i;
+	int score[N_STUDENT];
 	printf("input 5 scores:");
 	
-	sum =0;
 	for (i=0;i<N_STUDENT;i++)
-	{
-		scanf("%d",&grade[i]);
-		sum +=grade[i];
-	}
+		grade[i]=rand()%100+1;
 	
-	average=sum/N_STUDENT;
+	for(i=0;i<N_STUDENT;i++)
+		score[i]=grade[i];
 		
-	printf("score average:%i\n",average);
-		
+	for(i=0;i<N_STUDENT;i++)
+		printf("score[%d]=%d(grade:%i)\n",i,score[i],grade[i]);
 	return 0;
 }
